49d0e94799a9bb023797dd892e827b64058ac375
Dan Cowdry <medtc411@gmail.com>
2021-12-13 11:06:01 -0800

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CodingEvents_Exercise.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
